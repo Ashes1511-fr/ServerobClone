06c0825e4ce78a9ab7b6f8dad4d38a507f5c9fb1
Swapnil <swapnil13012005@gmail.com>
2026-06-23 13:09:15 +0530

    refactor header and content layout; remove unused pause button

--- script.js
+++ script.js
@@ -11,24 +11,80 @@ if (Hls.isSupported()) {
   video.src = videoSrc;
 }
 
+gsap.registerPlugin(ScrollTrigger);
+
 let lastscroll = 0;
+let header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    let currentscroll = window.pageYOffset;
+  let currentscroll = window.pageYOffset;
 
-    if (currentscroll > 300) {
-
-        if (currentscroll > lastscroll) {
-            gsap.to("header", { y: -100 });
-        }
-         else {
-            gsap.to("header", { y: 0 });
-        }
-
-    } 
-    else {
-        gsap.to("header", { y: 0 });
+  if (currentscroll > window.innerHeight * 0.5) {
+    if (currentscroll > lastscroll) {
+      gsap.to(header, {
+        y: -120,
+        duration: 0.4,
+        ease: "power2.out"
+      });
+    } else {
+      gsap.to(header, {
+        y: 0,
+        duration: 0.4,
+        ease: "power2.out"
+      });
     }
+  } else {
+    gsap.to(header, {
+      y: 0,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+  }
 
-    lastscroll = currentscroll;
+  lastscroll = currentscroll;
 });
+
+let h1 = document.querySelector(".page2 h1");
+
+let words = h1.innerHTML
+  .split("<br>")
+  .map(line =>
+    line
+      .trim()
+      .split(" ")
+      .map(word => `<span>${word}</span>`)
+      .join(" ")
+  )
+  .join("<br>");
+
+h1.innerHTML = words;
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page2",
+    start: "top top",
+    end: "+=1000",
+    scrub: 2,
+    pin: true,
+    anticipatePin: 1,
+    markers: false
+  }
+});
+
+tl.to(".rings", {
+  scale: 0.6,
+  filter: "blur(30px)",
+  opacity: 0.5,
+  duration: 1
+});
+
+tl.from(
+  ".page2 h1 span",
+  {
+    opacity: 0,
+    y: 30,
+    stagger: 0.08,
+    duration: 1
+  },
+  0.3
+);
